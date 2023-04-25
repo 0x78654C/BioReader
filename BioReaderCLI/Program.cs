a072991b3d63376819ec04807d74aa71fa20941a
x_Coding <xcoding.dev@gmail.com>
2023-04-25 21:40:15 +0300

    moved stuff to seperated classes

--- BioReaderCLI/Program.cs
+++ BioReaderCLI/Program.cs
@@ -1,55 +1,15 @@
-﻿using BioLib;
+﻿using BioReaderCLI.Utils;
+
+var ui = new UI(); 
 
 // Run if any arguments;
 if (args.Any())
-    ConvertToBioReader(args);
+{
+    var convert = new BioConvert(args);
+    convert.BioConverter();
+}
 else
-    ColorConsoleTextLineError("Need to provite a text for bionic reader conversion!");
-
-
-// Get paramas from args as a string.
-void ConvertToBioReader(string[] args)
-{
-    Console.WriteLine("\n\n\n--------------\n");
-    foreach (var arg in args)
-        if (!string.IsNullOrEmpty(arg))
-        {
-            var halfWords = Reader.GetHalfChars(arg);
-            foreach (var bioWrod in halfWords)
-            {
-                var secondHalf = bioWrod.Key.Substring(bioWrod.Value.Length);
-                ColorConsoleText(ConsoleColor.DarkGreen, bioWrod.Value);
-                ColorConsoleText(ConsoleColor.White, $"{secondHalf} ");
-            }
-        }
-    Console.WriteLine("\n\n--------------\nPress any key to exit...");
-    Console.ReadLine();
-}
+    ui.ColorConsoleTextLineError("Need to provite a text or file for bionic reader conversion!");
 
 
 
-/// <summary>
-/// Change color of a specific text in console.
-/// </summary>
-/// <param name="color"></param>
-/// <param name="text"></param>
-void ColorConsoleText(ConsoleColor color, string text)
-{
-    ConsoleColor currentForeground = Console.ForegroundColor;
-    Console.ForegroundColor = color;
-    Console.Write(text);
-    Console.ForegroundColor = currentForeground;
-}
-
-/// <summary>
-/// Change color of a specific text in console.
-/// </summary>
-/// <param name="color"></param>
-/// <param name="text"></param>
-void ColorConsoleTextLineError(string errorMessage)
-{
-    ConsoleColor currentForeground = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(errorMessage);
-    Console.ForegroundColor = currentForeground;
-}
